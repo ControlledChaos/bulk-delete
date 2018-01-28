0d2f6d45ac5ed1cb33d773f35a6c79636e9bfc99
Maria Daniel Deepak <mariadanieldeepak@gmail.com>
2018-01-28 09:34:53 +0530

    Remove duplicated render_page() method
    as it is handled in Base Page class

--- include/posts/class-bd-posts-page.php
+++ include/posts/class-bd-posts-page.php
@@ -60,52 +60,7 @@ class BD_Posts_Page extends BD_Page {
 		);
 	}
 
-	/**
-	 * Show the delete posts page.
-	 *
-     * @since 5.7.0
-	 */
-	public function render_page() {
-		?>
-		<div class="wrap">
-			<h2><?php _e( 'Bulk Delete Posts', 'bulk-delete' );?></h2>
-			<?php settings_errors(); ?>
 
-			<form method = "post">
-				<?php
-				// nonce for bulk delete
-				wp_nonce_field( 'sm-bulk-delete-posts', 'sm-bulk-delete-posts-nonce' );
-
-				/* Used to save closed meta boxes and their order */
-				wp_nonce_field( 'meta-box-order', 'meta-box-order-nonce', false );
-				wp_nonce_field( 'closedpostboxes', 'closedpostboxesnonce', false );
-				?>
-				<div id = "poststuff">
-					<div id="post-body" class="metabox-holder columns-1">
-
-						<div class="notice notice-warning">
-							<p><strong><?php _e( 'WARNING: Posts deleted once cannot be retrieved back. Use with caution.', 'bulk-delete' ); ?></strong></p>
-						</div>
-
-						<div id="postbox-container-2" class="postbox-container">
-							<?php do_meta_boxes( '', 'advanced', null ); ?>
-						</div> <!-- #postbox-container-2 -->
-
-					</div> <!-- #post-body -->
-				</div><!-- #poststuff -->
-			</form>
-		</div><!-- .wrap -->
-
-		<?php
-		/**
-		 * Runs just before displaying the footer text in the "Bulk Delete Posts" admin page.
-		 *
-		 * This action is primarily for adding extra content in the footer of "Bulk Delete Posts" admin page.
-		 *
-		 * @since 5.0.0
-		 */
-		do_action( 'bd_admin_footer_posts_page' );
-	}
 }
 
 new BD_Posts_Page();
